4d99f293fb766f9d6c574acb22785188746acc7d
Alireza <siralirezagh@gmail.com>
2023-10-14 15:21:33 -0400

    Loader added

--- src/components/hooks/loader.ts
+++ src/components/hooks/loader.ts
@@ -1,1 +1,33 @@
-    export default {}
+import {useEffect, useState} from "react";
+
+/**
+ * Custom hook to handle loading and error state while calling an async function or sending network requests.
+ * @param queryFunction
+ * @param initialData
+ * @param deps
+ */
+export default function useLoader<DataT>(queryFunction: () => (Promise<DataT> | DataT), initialData: any = null, deps = []) {
+    const [error, setError] = useState<Error | null>(null)
+    const [data, setData] = useState<DataT>(initialData)
+    const [status, setStatus] = useState<"idle" | "fetching" | "success" | "error">("idle")
+    useEffect(() => {
+        try {
+            (async () => {
+                setStatus("fetching")
+                const result = await queryFunction()
+                setData(result)
+                setStatus("success")
+            })()
+        } catch (e) {
+            setError(e as Error)
+            setStatus("success")
+        }
+    }, deps)
+    return {
+        isLoading: status === "fetching" || status === "idle",
+        error,
+        data,
+        status,
+        setData
+    }
+}
