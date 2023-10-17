b8eb8dc5260f621431b173a6ba64fced9b9cc1a8
Alireza <siralirezagh@gmail.com>
2023-10-17 20:45:07 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,31 +9,6 @@ const root = createRoot(rootElement);
 root.render(
     <StrictMode>
         <div>
-            <div className="container-fluid">
-                <h1> React Class Conversion</h1>
-                <ul>
-                    <li>
-                        Convert this TODO application from a class component into a function
-                        component using es6 and react hooks.
-                    </li>
-                    <li>
-                        Use{" "}
-                        <a href="https://styled-components.com/" target="_blank">
-                            styled components
-                        </a>{" "}
-                        for all styles
-                    </li>
-                    <li>Create a way to delete multiple selected items</li>
-                    <li>
-                        Create a way to save the lists you have created in an organized
-                        fashion. The ability to resume editing or delete a whole saved list
-                        is required.
-                    </li>
-                    <li>Bonus put your own design flair on this application!</li>
-                    <li>Have fun!</li>
-                </ul>
-            </div>
-
             <AppV2 />
         </div>
     </StrictMode>
